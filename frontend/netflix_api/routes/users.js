6d9e20493db0e2949c95da4b1c5e9bc1782c8e22
dhananjay1401 <gaurdhananjay54@gmail.com>
2022-08-06 20:42:28 +0530

    changes

--- frontend/netflix_api/routes/users.js
+++ frontend/netflix_api/routes/users.js
@@ -53,18 +53,58 @@ router.get("/find/:id", async (req, res) => {
 });
 // GET ALL
 router.get("/", verify, async (req, res) => {
-    const query = req.query.new;
-    if (req.user.isAdmin) {
-      try {
-        const users = query ? await User.find().limit() : await User.find();
-        res.status(200).json("User has been deleted");
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    } else {
-      res.status(403).json("You are not allowed to see all users!");
+  const query = req.query.new;
+  if (req.user.isAdmin) {
+    try {
+      const users = query
+        ? await User.find().sort({ _id: -1 }).limit(4)
+        : await User.find();
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json(err);
     }
-  });
+  } else {
+    res.status(403).json("You are not allowed to see all users!");
+  }
+});
 // GET USER STATS
+router.get("/stats", async (req, res) => {
+  const today = new Date();
+  const lastYear = today.setFullYear(today.setFullYear - 1);
+
+  const monthsArray = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  try {
+    const data = await User.aggregate([
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1},
+        },
+      },
+    ]);
+    res.status(200).json(data)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
