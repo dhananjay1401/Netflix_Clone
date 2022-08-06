a6b88cd52a61f497fb317a9ddc0af7f0452a41c4
dhananjay1401 <gaurdhananjay54@gmail.com>
2022-08-06 19:14:10 +0530

    changes

--- frontend/netflix_api/routes/users.js
+++ frontend/netflix_api/routes/users.js
@@ -4,7 +4,6 @@ const CryptoJS = require("crypto-js");
 const verify = require("../verifyToken");
 
 //UPDATE
-
 router.put("/:id", verify, async (req, res) => {
   if (req.user.id === req.params.id || req.user.isAdmin) {
     if (req.body.password) {
@@ -30,8 +29,42 @@ router.put("/:id", verify, async (req, res) => {
   }
 });
 // DELETE
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User has been deleted");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You can update only your account!");
+  }
+});
 // GET
+router.get("/find/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...info } = user._doc;
+    res.status(200).json(info);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 // GET ALL
+router.get("/", verify, async (req, res) => {
+    const query = req.query.new;
+    if (req.user.isAdmin) {
+      try {
+        const users = query ? await User.find().limit() : await User.find();
+        res.status(200).json("User has been deleted");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(403).json("You are not allowed to see all users!");
+    }
+  });
 // GET USER STATS
 
 module.exports = router;
