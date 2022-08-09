ed00cec9589eea86f9a496c2892a764159e6920f
dhananjay1401 <gaurdhananjay54@gmail.com>
2022-08-10 01:05:19 +0530

    login added

--- frontend/netflix_admin/src/App.js
+++ frontend/netflix_admin/src/App.js
@@ -1,4 +1,3 @@
-
 // Compoents
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Topbar from "./Components/Topbar/Topbar";
@@ -11,6 +10,7 @@ import NewUser from "./Pages/NewUser/NewUser";
 import ProductList from "./Pages/ProductList/ProductList";
 import Product from "./Pages/Product/Product";
 import NewProduct from "./Pages/NewProduct/NewProduct";
+import Login from "./Pages/Login/Login";
 
 import "./app.css";
 
@@ -19,25 +19,30 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-  
   return (
     <>
-      <Router className="App">
+      <Router>
+        <Routes>
+          
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+      {/* <Router className="App">
         <header className="App-header"></header>
         <Topbar />
         <div className="container">
           <Sidebar />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/users" element={<UsersList />} />
-            <Route path="/user" element={<User />} />
-            <Route path="/newuser" element={<NewUser />} />
-            <Route path="/products" element={<ProductList />} />
-            <Route path="/product" element={<Product />} />
-            <Route path="/newproduct" element={<NewProduct />} />
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/users" element={<UsersList />} />
+            <Route exact path="/user" element={<User />} />
+            <Route exact path="/newuser" element={<NewUser />} />
+            <Route exact path="/products" element={<ProductList />} />
+            <Route exact path="/product" element={<Product />} />
+            <Route exactpath="/newproduct" element={<NewProduct />} />
           </Routes>
         </div>
-      </Router>
+      </Router> */}
     </>
   );
 }
--- frontend/netflix_admin/src/Pages/Login/Login.jsx
+++ frontend/netflix_admin/src/Pages/Login/Login.jsx
@@ -7,6 +7,7 @@ function Login() {
         <form className="loginForm">
             <input type="text" placeholder="email" className="loginInput" />
             <input type="password" placeholder="password" className="loginInput" />
+            <button className='loginButton'>Login</button>
         </form>
     </div>
     </>
