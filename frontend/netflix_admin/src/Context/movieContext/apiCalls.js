88923893cda57c8018f61758b0c8351631b30eac
dhananjay1401 <gaurdhananjay54@gmail.com>
2022-08-14 23:45:34 +0530

    commit

--- frontend/netflix_admin/src/Context/movieContext/apiCalls.js
+++ frontend/netflix_admin/src/Context/movieContext/apiCalls.js
@@ -24,3 +24,33 @@ export const getMovies = async (dispatch) => {
     dispatch(getMoviesFailure());
   }
 };
+
+//create
+export const createMovie = async (movie, dispatch) => {
+  dispatch(createMovieStart());
+  try {
+    const res = await axios.post("/movies", movie, {
+      headers: {
+        token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+      },
+    });
+    dispatch(createMovieSuccess(res.data));
+  } catch (err) {
+    dispatch(createMovieFailure());
+  }
+};
+
+//delete
+export const deleteMovie = async (id, dispatch) => {
+  dispatch(deleteMovieStart());
+  try {
+    await axios.delete("/movies/" + id, {
+      headers: {
+        token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+      },
+    });
+    dispatch(deleteMovieSuccess(id));
+  } catch (err) {
+    dispatch(deleteMovieFailure());
+  }
+};
